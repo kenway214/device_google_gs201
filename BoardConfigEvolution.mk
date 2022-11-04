2d53bbb16b29403ed851a5cd9f3f99c1a0dec972
AnierinB <anierin@evolution-x.org>
2022-11-04 20:46:06 +0000

    gs201: Initialize Evolution X 8.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfigEvolution.mk
+++ BoardConfigEvolution.mk
@@ -17,12 +17,8 @@ TARGET_NEEDS_DTBOIMAGE := true
 BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := device/google/gs201/vendor_dlkm.modules.blocklist
 TARGET_KERNEL_EXT_MODULE_ROOT := kernel/google/gs201/private/google-modules
 
-# Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := true
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := false
-
 # Manifests
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/evolution/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += device/google/gs201/android.hardware.security.rkp-service.citadel.xml
 ifneq ($(BOARD_WITHOUT_RADIO),true)
 DEVICE_MANIFEST_FILE += device/google/gs201/manifest_radio_ds.xml
@@ -46,9 +42,6 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 
 AB_OTA_PARTITIONS += \
     vbmeta_vendor
-
-# Reserve space for gapps install
--include vendor/lineage/config/BoardConfigReservedSize.mk
 
 # Verified Boot
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
