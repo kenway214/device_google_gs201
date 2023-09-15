e8e87cfe479bbd1262184154331e0fd10cd302c2
alk3pInjection <webmaster@raspii.tech>
2023-09-15 17:12:24 +0800

    gs201: Allow running 32-bit apps
    Change-Id: Ibf1e2d8313b0a2fd09bb0cf4c17f2774f20ee15a

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -32,9 +32,6 @@ TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a55
 TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
-# Enable 64-bit for non-zygote.
-ZYGOTE_FORCE_64 := true
-
 # Force any prefer32 targets to be compiled as 64 bit.
 IGNORE_PREFER32_ON_DEVICE := true
 
